5a580b3af10ac5f3938bb4faa59a37d96002bbd5
negindorali <vendraa.78@gmail.com>
2022-05-22 23:05:53 +0430

    fix bugs

--- resource/js/components/mediaAdventure.vue
+++ resource/js/components/mediaAdventure.vue
@@ -85,7 +85,7 @@
                             <p><span>{{ channel.following }}</span><small> :Following</small></p>
                         </div>
 
-                        <div class="middle align-content-center">
+                        <div :class="bgCOlors">
                             <p v-html="getPrice(channel)"></p>
                             <p>آخرین بروزرسانی : {{ channel.last_update }}</p>
                             <div class="hero-btn">
@@ -110,10 +110,15 @@
 
 <script>
 export default {
+
     name: "mediaAdventure",
     data() {
         return {
             channels: [],
+            bgCOlors:[
+              'INsta',
+              'middle'
+            ],
             responsive:'',
             page: 0,
             grow: "",
@@ -121,7 +126,7 @@ export default {
             selected:[],
             province: [],
             blackList:[],
-            types: "",
+            types: "اینستاگرام",
             work_category: [],
             categories: [],
             selected_category: [],
@@ -194,6 +199,12 @@ export default {
         page() {
             if (this.page > 0)
                 this.getChannels();
+        },
+        types(){
+            if(this.types==="INSTAGRAM" || this.types==="INFLUENCER")
+                this.bgCOlors = "middle"
+            else
+                this.bgCOlors = "INsta"
         }
     },
     computed: {
@@ -204,7 +215,6 @@ export default {
             if (this.selected_category.length > 0 && this.types !== '')
                 this.getChannels()
         },
-
     },
     mounted() {
         window.observer = new IntersectionObserver((entries) => {
@@ -244,5 +254,286 @@ export default {
 </script>
 
 <style scoped>
+.hero-section {
+    background-size: cover;
+    height: 33rem;
+    bottom: 0;
+    width: 100%;
+    padding: 20px;
+}
+
+.sabt-input {
+    border-radius: 5px;
+    font-size: 10pt;
+    background-color: rgba(191, 215, 237, 1);
+    border-color: rgba(191, 215, 237, 1);
+    height: 2.5rem;
+}
+
+.sabt-section {
+    border-radius: 5px;
+    top: -4.5rem;
+    right: -3%;
+    box-shadow: 5px 10px 10px rgba(191, 215, 237, 0.20);
+}
+
+.btn-sabt {
+    background-image: linear-gradient(to right, #C04848 0%, #480048  51%, #C04848  100%);
+    transition: 0.5s;
+    background-size: 200% auto;
+    border-radius: 8px;
+    box-shadow: 5px 10px 10px rgba(191, 215, 237, 0.20);
+}
+
+.sabt-section-phone {
+    border-radius: 5px;
+    top: -5rem;
+    right: 8.5%;
+    box-shadow: 5px 10px 10px rgba(191, 215, 237, 0.20);
+    background-color: rgba(242, 242, 242, 1);
+}
+
+.btn-sabt:hover {
+    background-position: right center; /* change the direction of the change here */
+    color: #fff;
+    text-decoration: none;
+}
+
+.targets {
+    background-color: #055C9D !important;
+    color: white !important;
+    border-radius: 5px;
+}
+
+.zoom {
+    transition: transform .2s; /* Animation */
+    margin: 0 auto;
+}
+
+.zoom:hover {
+    transform: scale(1.5); /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
+    z-index: 99999;
+}
+
+/*.background-shape {*/
+/*    background-image: url("/img/logo-01.png");*/
+/*    width: 121rem;*/
+/*    height: 167rem;*/
+/*    min-height: auto;*/
+/*    -webkit-transform: skewY(-11deg);*/
+/*    position: absolute;*/
+/*    top: -18rem;*/
+/*    left: -30%;*/
+/*    opacity: 0.1;*/
+/*}*/
+
+.background-shape {
+    background: rgba(191, 215, 237, 0.20);
+    width: 120rem;
+    height: 85rem;
+    -ms-transform: skewY(-20deg);
+    -webkit-transform: skewY(25deg);
+    position: absolute;
+    top: 57rem;
+    left: 0;
+}
+
+.icons i:hover {
+    margin-right: 10rem;
+}
+
+#space {
+    transition: all .7s;
+}
+
+.rpZizi:hover #space {
+    margin-right: .5rem;
+}
+
+.special {
+    font-weight: bold !important;
+    color: #fff !important;
+    background: #bc0000 !important;
+    text-transform: uppercase;
+    padding: 10px;
+}
+
+.overflow-auto {
+    scrollbar-width: none; /* for Firefox */
+    rollbar-width: none;
+}
+
+*::-webkit-scrollbar {
+    display: none; /* for Chrome, Safari, and Opera */
+}
+
+.hero {
+    display: inline-block;
+    position: relative;
+    height: 400px;
+    border-radius: 30px;
+    overflow:hidden;
+    box-shadow: 5px 5px 30px rgba(0, 0, 0, 0.3);
+}
+
+.hero:hover .middle{
+    opacity: 1;
+    height: 100%;
+    width: 100%;
+}
+
+.middle{
+    transition: .5s ease;
+    opacity: 0;
+    position: absolute;
+    width: 100%;
+    height: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-size: 200% auto;
+    color: white;
+    text-align: center;
+    font-weight: 900;
+    background-image: linear-gradient(to right, #AA076B 0%, #61045F  51%, #AA076B  100%);
+    padding-top: 5rem;
+}
+
+.middle:hover{
+    transition: .7s;
+    background-position: right center;
+}
+
+.hero:hover .INsta{
+    opacity: 1;
+    height: 100%;
+    width: 100%;
+}
+
+.INsta{
+    transition: .5s ease;
+    opacity: 0;
+    position: absolute;
+    width: 100%;
+    height: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-size: 200% auto;
+    color: black;
+    text-align: center;
+    font-weight: 900;
+    /*background-image: linear-gradient(to right, #1FA2FF 0%, #12D8FA  51%, #1FA2FF  100%);*/
+    background-image: linear-gradient(to right, #1FA2FF 0%, #12D8FA  51%, #1FA2FF  100%);
+    padding-top: 5rem;
+}
+
+.INsta:hover{
+    transition: .7s;
+    background-position: right center;
+}
+
+.hero-profile-img {
+    height: 70%;
+}
+
+.TELEGRAM {
+    background-image: linear-gradient(to right, #1FA2FF 0%, #12D8FA  51%, #1FA2FF  100%);
+    border-radius: 30px;
+    position: absolute;
+    top: 51%;
+    left: -5px;
+    height: 65%;
+    width: 108%;
+    transform: skew(19deg, -9deg);
+    transition: 0.5s;
+    background-size: 200% auto;
+}
+
+.TELEGRAM:hover {
+    background-position: right center; /* change the direction of the change here */
+    color:black;
+    text-decoration: none;
+    transition: 0.5s;
+}
+
+.INSTAGRAM, .INFLUENCER {
+    background-image: linear-gradient(to right, #AA076B 0%, #61045F  51%, #AA076B  100%);
+    border-radius: 30px;
+    position: absolute;
+    top: 51%;
+    left: -5px;
+    height: 65%;
+    width: 108%;
+    transform: skew(19deg, -9deg);
+    transition: 0.5s;
+    background-size: 200% auto;
+}
+
+.INSTAGRAM:hover, .INFLUENCER:hover {
+    background-position: right center; /* change the direction of the change here */
+    color: #fff;
+    text-decoration: none;
+    transition: 0.5s;
+}
+
+.zizigolo {
+    background-image: linear-gradient(to right, #360033 0%, #0b8793  51%, #360033  100%);
+    border-radius: 30px;
+    position: absolute;
+    top: 51%;
+    left: -5px;
+    height: 65%;
+    width: 108%;
+    transform: skew(19deg, -9deg);
+    transition: 0.5s;
+    background-size: 200% auto;
+}
+
+.zizigolo:hover {
+    background-position: right center; /* change the direction of the change here */
+    color: #fff;
+    text-decoration: none;
+    transition: 0.5s;
+}
+
+
+.hero-logo {
+    height: 60px;
+    width: 60px;
+    border-radius: 20px;
+    background-color: darkblue;
+    position: absolute;
+    bottom: 38%;
+    left: 30px;
+    overflow:hidden;
+    box-shadow: 5px 5px 30px rgba(0, 0, 0, 0.7);
+}
+
+.hero-logo img {
+    height: 100%;
+}
+
+.hero-description {
+    position: absolute;
+    color: #fff;
+    bottom: 4%;
+    right: 3%;
+    font-weight: 900;
+}
+
+.hero-btn {
+    position: absolute;
+    color: #fff;
+    left: 20px;
+    bottom: 14%;
+    padding: 0px 13px;
+    border: 1px solid #fff;
+}
+
+.hero-btn a {
+    color: #fff;
+}
+
 
 </style>
